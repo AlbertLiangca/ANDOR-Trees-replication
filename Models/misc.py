bc221aab35c70dc7f5b88ef1014457bf8c687bd9
Albert Liang <albert.liang.ca@gmail.com>
2026-07-31 11:15:18 -0700

    - Holy shit I can make a nll function out of participant data and minimize it - Up next: Cleaning up duplicates from processed data

--- Models/misc.py
+++ Models/misc.py
@@ -1,3 +1,6 @@
+import copy
+import numpy as np
+
 # I made a move class just to make my life easier when it comes to playing out a problem
 class move:
     def __init__(self,
@@ -17,3 +20,29 @@ class move:
     
     def display(self):
         return (self.id*(self.id != '-1')) + ('r'*int(self.id == '-1')) + str(self.move)
+
+def make_lambda_func(a,d):
+        return lambda y : a*((1-y)**d)
+    
+def make_adder(funcs):
+# Copy the function list 
+    funcs = funcs[:]
+    def sigma(x):
+        return sum(f(x) for f in funcs)
+    return sigma
+
+def make_reciprocal(func):
+    funcy = copy.copy(func)
+    def sigma(x):
+        return 1 - funcy(x)
+    return sigma
+
+def apply_neg_log(func):
+    funcy = copy.copy(func)
+    def sigma(x):
+        return -1*np.log(funcy(x))
+    return sigma
+
+def create_nll(ls):
+    funcs = ls[:]
+    return make_adder(list(apply_neg_log(l) for l in funcs))
